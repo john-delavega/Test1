a7680340db1928ebe06265143827abc86641642b
John Delavega <john-delavega@gmail.com>
2016-10-31 16:08:00 -0700

    Version 0.00 - initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Test1
 test app using Android Studio 2.2.2
+
+
+
+
+Version 0.00    2016-10-31
---
+++ app/src/main/java/us/delavega/john/test1/MainActivity.java
@@ -0,0 +1,48 @@
+package us.delavega.john.test1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import android.util.Log;
+import android.view.Menu;
+import android.widget.Button;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        //getMenuInflater().inflate(R.menu.main, menu);
+        Button button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+          //      Toast.makeText(MainActivity.this, "Button Clicked",	Toast.LENGTH_SHORT).show();
+                double x = Math.random();
+
+             //   System.out.print("test1");
+              //  android.util.Log.d ("test1");
+                Log.d("random = ", Double.toString(x));
+
+            }
+
+        });
+        return true;
+    }
+
+
+
+
+}
