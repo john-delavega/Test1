ea57a0129f593e117f2a09855ca81ea7886dd39b
John Delavega <john-delavega@gmail.com>
2016-11-01 17:46:13 -0700

    Version 0.03, textViewRandom

--- README.md
+++ README.md
@@ -5,6 +5,11 @@ test app using Android Studio 2.2.2
 
 
 
+Version 0.03    2016-11-01
+
+ - textViewRandom
+ 
+
 Version 0.02 
 
 Version 0.01 
--- app/src/main/java/us/delavega/john/test1/MainActivity.java
+++ app/src/main/java/us/delavega/john/test1/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.widget.Button;
+import android.widget.TextView;
 import android.view.View;
 import android.view.View.OnClickListener;
 
@@ -25,12 +26,18 @@ public class MainActivity extends Activity {
         //getMenuInflater().inflate(R.menu.main, menu);
         Button button = (Button) findViewById(R.id.button);
 
+        final TextView textView  = (TextView) findViewById(R.id.textViewRandom);
+
+        //textView.setText("test");
+
         button.setOnClickListener(new OnClickListener() {
 
             @Override
             public void onClick(View view) {
           //      Toast.makeText(MainActivity.this, "Button Clicked",	Toast.LENGTH_SHORT).show();
                 double x = Math.random();
+
+                textView.setText(Double.toString(x)); // requires final declaration
 
              //   System.out.print("test1");
               //  android.util.Log.d ("test1");
